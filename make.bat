bc5b28ec38b477bbdafec23744f98db84bd81119
Damian Bushong <katana@odios.us>
2018-08-03 11:07:04 -0500

    update for latest makefile from smtk

--- make.bat
+++ make.bat
@@ -1,6 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion enableextensions
+set iserror=0
 call _smtkpath.bat
 call "%smtkpath%\smtk.bat" %cd%
-endlocal & ( set smtkpath= )
+if errorlevel 1 (
+  set iserror=1
+)
+endlocal & (
+  set smtkpath=
+  set _iserror=%iserror%
+)
 pause
+exit /b %_iserror%
